e1578621dc6d538aad0d15a0a4a67b8e13198b4b
Aishwarya Reddy <118329497+aishreddy13@users.noreply.github.com>
2025-02-05 17:50:22 -0500

    Time: 39 ms (16.6%), Space: 20.2 MB (49.27%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -6,7 +6,6 @@ class Solution:
             if nums[x] == 1:
                 sum_1 +=1
             else:
-                longest_1 = max(sum_1, longest_1) 
                 sum_1 = 0
-            
-        return max(longest_1, sum_1)   
+            longest_1 = max(sum_1, longest_1) 
+        return longest_1    
