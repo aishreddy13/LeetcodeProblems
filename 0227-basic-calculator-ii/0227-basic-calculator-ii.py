9f182abe8f0d040ddfe99f1d099ec138b0d8935c
Aishwarya Reddy <118329497+aishreddy13@users.noreply.github.com>
2025-02-18 17:02:57 -0500

    Time: 67 ms (64.42%), Space: 18.9 MB (91.88%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,21 +2,26 @@ class Solution:
     def calculate(self, s: str) -> int:
         if not s:
             return 0
-        stack =[]
-        curr_number = 0
-        operation ='+'
+
+        length = len(s)
+        current_number = 0
+        last_number = 0
+        result = 0
+        operation = '+'
+
         for i, char in enumerate(s):
             if char.isdigit():
-                curr_number = curr_number * 10 + int(char)
-            if (not char.isdigit() and not char.isspace()) or i == len(s) -1:
-                if operation == '-':
-                    stack.append(-curr_number)
-                elif operation == '+':
-                    stack.append(curr_number)
+                current_number = current_number * 10 + int(char)
+            if (not char.isdigit() and not char.isspace()) or i == length - 1:
+                if operation == '+' or operation == '-':
+                    result += last_number
+                    last_number = current_number if operation == '+' else -current_number
                 elif operation == '*':
-                    stack.append(stack.pop() * curr_number)
+                    last_number = last_number * current_number
                 elif operation == '/':
-                    stack.append(int(stack.pop() / curr_number))
+                    last_number = int(last_number / current_number)
                 operation = char
-                curr_number = 0
-        return sum(stack)
+                current_number = 0
+
+        result += last_number
+        return result
