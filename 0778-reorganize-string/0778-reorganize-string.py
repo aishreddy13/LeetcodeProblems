c8c9d9408599c6c4bf2c3d4c34aa4603acfeb97b
Aishwarya Reddy <118329497+aishreddy13@users.noreply.github.com>
2025-06-22 01:31:06 -0500

    Time: 1 ms (63.38%), Space: 18 MB (7.32%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,29 +1,22 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        char_counts = Counter(s)
-        max_count, letter = 0, ''
-        for char, count in char_counts.items():
-            if count > max_count:
-                max_count = count
-                letter = char
-        if max_count > (len(s) + 1) // 2:
-            return ""
-        ans = [''] * len(s)
-        index = 0
+        ans = []
+        pq = [(-freq, char) for char, freq in Counter(s).items()]
+        heapq.heapify(pq)
 
-        # Place the most frequent letter
-        while char_counts[letter] != 0:
-            ans[index] = letter
-            index += 2
-            char_counts[letter] -= 1
+        while len(pq) >= 2:
+            freq1, char1 = heappop(pq)
+            freq2, char2 = heappop(pq)
 
-        # Place rest of the letters in any order
-        for char, count in char_counts.items():
-            while count > 0:
-                if index >= len(s):
-                    index = 1
-                ans[index] = char
-                index += 2
-                count -= 1
+            ans.extend([char1, char2])
+            if freq1 + 1 < 0:
+                heapq.heappush(pq, (freq1 + 1, char1))
+            if freq2 + 1 < 0:
+                heapq.heappush(pq, (freq2+1, char2))
+        if pq:
+            freq, char = heapq.heappop(pq)
+            if -freq > 1:
+                return ""
+            ans.append(char)
 
         return ''.join(ans)
